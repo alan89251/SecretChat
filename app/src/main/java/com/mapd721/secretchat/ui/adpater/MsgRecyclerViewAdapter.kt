e1db712f1209eeee3fa0a311110d23c6e89cec72
Alan <csuen2@my.centennialcollege.ca>
2023-03-21 20:41:09 -0400

    Fix chat bubble layout

--- app/src/main/java/com/mapd721/secretchat/ui/adpater/MsgRecyclerViewAdapter.kt
+++ app/src/main/java/com/mapd721/secretchat/ui/adpater/MsgRecyclerViewAdapter.kt
@@ -1,72 +1,51 @@
 package com.mapd721.secretchat.ui.adpater
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.mapd721.secretchat.data_model.chat.Message
-import com.mapd721.secretchat.databinding.RecyclerViewReceiverBinding
-import com.mapd721.secretchat.databinding.RecyclerViewSenderBinding
+import com.mapd721.secretchat.databinding.RecyclerViewChatBinding
 import java.text.SimpleDateFormat
 
 class MsgRecyclerViewAdapter(
     var msgList: List<Message>
-): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    class SenderViewHolder(itemBinding: RecyclerViewSenderBinding)
+): RecyclerView.Adapter<MsgRecyclerViewAdapter.ViewHolder>() {
+    class ViewHolder(itemBinding: RecyclerViewChatBinding)
         : RecyclerView.ViewHolder(itemBinding.root) {
-            val binding: RecyclerViewSenderBinding = itemBinding
+        val binding: RecyclerViewChatBinding = itemBinding
         }
 
-    class ReceiverViewHolder(itemBinding: RecyclerViewReceiverBinding)
-        : RecyclerView.ViewHolder(itemBinding.root) {
-            val binding: RecyclerViewReceiverBinding = itemBinding
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: RecyclerViewChatBinding = RecyclerViewChatBinding.inflate(
+            LayoutInflater.from(parent.context)
+        )
+
+        return ViewHolder(binding)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when (viewType) {
-            VIEW_TYPE_SENDER -> {
-                val binding: RecyclerViewSenderBinding = RecyclerViewSenderBinding.inflate(
-                    LayoutInflater.from(parent.context))
-                SenderViewHolder(binding)
-            }
-            else -> { // Receiver
-                val binding: RecyclerViewReceiverBinding = RecyclerViewReceiverBinding.inflate(
-                    LayoutInflater.from(parent.context))
-                ReceiverViewHolder(binding)
-            }
-        }
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // init layout
+        holder.binding.senderDialog.visibility = View.GONE
+        holder.binding.receiverDialog.visibility = View.GONE
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val message = msgList[position]
         val dataFormat = SimpleDateFormat("hh:mm")
-        when (holder.itemViewType) {
-            VIEW_TYPE_SENDER -> {
-                val concreteHolder = holder as SenderViewHolder
-                concreteHolder.binding.contentText.text = message.text
-                concreteHolder.binding.msgTime.text = dataFormat.format(message.sentDateTime)
+        when (message.type) {
+            Message.TYPE_SNED -> {
+                holder.binding.senderDialog.visibility = View.VISIBLE
+                holder.binding.senderText.text = message.text
+                holder.binding.senderMsgTime.text = dataFormat.format(message.sentDateTime)
             }
-            VIEW_TYPE_RECEIVER -> {
-                val concreteHolder = holder as ReceiverViewHolder
-                concreteHolder.binding.contentText.text = message.text
-                concreteHolder.binding.msgTime.text = dataFormat.format(message.sentDateTime)
+            else -> { // RECEIVE
+                holder.binding.receiverDialog.visibility = View.VISIBLE
+                holder.binding.receiverText.text = message.text
+                holder.binding.receiverTime.text = dataFormat.format(message.sentDateTime)
             }
         }
     }
 
     override fun getItemCount(): Int {
         return msgList.size
-    }
-
-    override fun getItemViewType(position: Int): Int {
-        val message = msgList[position]
-        return when(message.type) {
-            Message.TYPE_SNED -> VIEW_TYPE_SENDER
-            else -> VIEW_TYPE_RECEIVER
-        }
-    }
-
-    companion object {
-        const val VIEW_TYPE_SENDER = 0
-        const val VIEW_TYPE_RECEIVER = 1
     }
 }

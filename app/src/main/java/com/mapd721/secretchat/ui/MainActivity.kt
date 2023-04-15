149342feffe5bc5c845c888f48e4fa50d4cb00e5
Alan <csuen2@my.centennialcollege.ca>
2023-04-15 17:40:31 -0400

    Add navigation back support

--- app/src/main/java/com/mapd721/secretchat/ui/MainActivity.kt
+++ app/src/main/java/com/mapd721/secretchat/ui/MainActivity.kt
@@ -2,15 +2,31 @@ package com.mapd721.secretchat.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.mapd721.secretchat.R
+import com.mapd721.secretchat.databinding.ActivityMainBinding
 import com.mapd721.secretchat.ui.view_model.GlobalViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     private val globalViewModel: GlobalViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                findNavController(binding.navHostFragment.id)
+                    .navigateUp()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
